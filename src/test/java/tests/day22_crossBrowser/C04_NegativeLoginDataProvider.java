e4f6bb4e295c21f7d1c17c27b20a213a86392836
Mehmet Magden <yasamtr@gmail.com>
2022-06-04 00:50:47 +0300

    BackUp

--- src/test/java/tests/day22_crossBrowser/C04_NegativeLoginDataProvider.java
+++ src/test/java/tests/day22_crossBrowser/C04_NegativeLoginDataProvider.java
@@ -1,27 +1,36 @@
 package tests.day22_crossBrowser;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pages.BrcPage;
 import utilies.ConfigReader;
 import utilies.Driver;
 
 public class C04_NegativeLoginDataProvider {
-    BrcPage brcPage = new BrcPage();
+    BrcPage brcPage ;
+    @DataProvider
+    public static Object[][] kullaniciListesi() {
+        Object[][] kullaniciBilgileri= {{"firuze@nehaber.com","12354"},
+                                        {"murat@ben.com","65874"},
+                                        {"ilyas@asdf.com","987654"      } };
+        return kullaniciBilgileri;
+    }
 
-    @Test
-    public void yanlisSifre() {
+    @Test(dataProvider = "kullaniciListesi")
+    public void yanlisSifre(String userEmail,String password) {
 
+        brcPage = new BrcPage();
         // Bir test method olustur positiveLoginTest()
         //       https://www.bluerentalcars.com/ adresine git
         Driver.getDriver().get(ConfigReader.getProperty("brcUrl"));
         //      login butonuna bas
         brcPage .ilkLoginButtonu.click();
 
-        //test data user email: customer@bluerentalcars.com ,
-        brcPage.emailTextBox.sendKeys(ConfigReader.getProperty("brcValidEmail"));
-        //test data password : 54321
-        brcPage.passwordTextBox.sendKeys(ConfigReader.getProperty("brcWrongdPassword"));
+        //test data user email: dataProvider'dan alalım ,
+        brcPage.emailTextBox.sendKeys(userEmail);
+        //test data password : dataProvider'dan alalım ,
+        brcPage.passwordTextBox.sendKeys(password);
         //goin buttonuna tıklayın
         brcPage.ikinciLoginButtonu.click();
         //Degerleri girildiginde sayfaya basarili sekilde girilemediğini test et
